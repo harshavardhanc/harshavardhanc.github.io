4ddc7fea949f28189535c0a4279057fb0e4aeb24
harshavardhanc <harshavardhanc95@gmail.com>
2018-05-07 22:48:03 +0530

    Updating script in feature branch

--- ping.sh
+++ ping.sh
@@ -2,7 +2,10 @@
 ip=$1
   a=$(ping -c1 $1)
 
-if [ $? -eq 0 ]
+if [ $# -eq 0 ]
+then 
+ echo "usage: $0 <ip_address>"
+elif [ $? -eq 0 ]
 then
  echo "Server is reachable"
 else
--- reverse.sh
+++ reverse.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-
+echo "Welcome to reverse string"
 echo "Enter a word or number"
 read string
 echo $string | rev
