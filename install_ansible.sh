a4c074ab63751f386d56e236238f5177d048fb74
harshavardhanc <harshavardhanc95@gmail.com>
2018-05-17 17:31:30 +0530

    modified script

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get update -y
-sudo apt-get install python-pip -y
+apt-get update -y
+apt-get install python-pip -y
 
-sudo pip install ansible==2.4.3
+pip install ansible==2.4.3
